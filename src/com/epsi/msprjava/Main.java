88681d29c5bedd43c595a455179c1edfefd9e637
pampletousse <sloppin23@gmail.com>
2020-04-07 12:29:19 +0200

    Mise en place frame de connexion:labels,inputs,btns

--- src/com/epsi/msprjava/Main.java
+++ src/com/epsi/msprjava/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("views/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("views/connexion.fxml"));
+        primaryStage.setTitle("RECYCL - Monitoring");
+        primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.show();
     }
 
